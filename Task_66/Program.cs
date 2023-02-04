b6caa68e4259e7e667a2f163788f651a57976fe6
Алексей Верещагин <aleksejveresagin@MacBook-Pro-Aleksej.local>
2023-02-04 11:05:10 +0500

    add Task_66 summ of natural elements

---
+++ Task_66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+     int SumNumbers(int n, int m) 
+{ 
+     if (n != m) 
+     {
+         return (n+SumNumbers(n - 1, m)); 
+     } 
+  else return n;
+}
+    Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = SumNumbers(n, m); 
+Console.Write(sum);
